0e3e2e4d195d974173227a209a9a6c2097bef967
Marseel-E <74118311+Marseel-E@users.noreply.github.com>
2022-03-11 09:05:01 -0500

    Create test_slash.py

---
+++ cogs/test_slash.py
@@ -0,0 +1,19 @@
+from discord import Interaction, Object
+from discord.app_commands import command
+from discord.ext.commands import Cog
+
+
+test_guild = Object(id=843994109366501376)
+
+class Test_slash(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @command(guild=test_guild)
+    async def test(self, interaction: Interaction):
+        await interaction.response.send_message("works", ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(Test_slash(bot))
