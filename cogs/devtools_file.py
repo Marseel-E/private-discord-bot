4ef451020c72fb26fed6b33c06eff2015918625a
Marseel-E <74118311+Marseel-E@users.noreply.github.com>
2022-03-27 20:55:53 -0400

    Add files via upload

---
+++ cogs/devtools_file.py
@@ -0,0 +1,91 @@
+import os
+
+from discord.app_commands import Group, command, check
+from discord import Interaction, TextStyle
+from discord.ext.commands import Cog, Bot
+from discord.ui import Modal, TextInput
+
+from utils import test_server, is_owner
+
+
+async def create_and_write(interaction: Interaction, name: str, content: str) -> None:
+	FILE_PATH = f'dev_files/{name}'
+
+	if not (content):
+		await interaction.response.send_message(":x: Cannot create an empty file", ephemeral=True)
+		return
+
+	with open(FILE_PATH, 'w+') as f:
+		f.write(content)
+
+	await interaction.response.send_message(f":white_check_mark: Written `{os.path.getsize(FILE_PATH)}` bytes to '{self.name}'", ephemeral=True)
+
+def get_file_content(name: str) -> str:
+	with open(f'dev_files/{name}', 'r') as f:
+		return f.read()
+
+
+class Create_file(Modal):
+	def __init__(self, file_name: str) -> None:
+		self.file_name = file_name
+		
+		super().__init__(title="Create file", timeout=300.0)
+
+		self.add_item(TextInput(label=f"{self.file_name} <NEW>", style=TextStyle.paragraph, placeholder="File content"))
+
+	async def on_submit(self, interaction: Interaction):
+		await create_and_write(interaction, self.file_name, self.children[0].value)
+
+
+class Edit_file(Modal):
+	def __init__(self, file_name: str) -> None:
+		self.file_name = file_name
+		
+		super().__init__(title="Edit file", timeout=300.0)
+
+		self.add_item(TextInput(label=f"{self.file_name}", style=TextStyle.paragraph, placeholder="File content", default=get_file_content(self.file_name)))
+
+	async def on_submit(self, interaction: Interaction):
+		await create_and_write(interaction, self.file_name, self.children[0].value)
+
+
+class File(Cog, Group, name="file"):
+	def __init__(self, bot: Bot) -> None:
+		self.bot = bot
+		super().__init__()
+
+	async def interaction_check(self, interaction: Interaction) -> bool:
+		return await is_owner(interaction)
+
+
+	@command()
+	async def create(self, interaction: Interaction, name: str, content: str = None):
+		if not (content):
+			await interaction.response.send_modal(Create_file(name))
+			return
+
+		await create_and_write(interaction, name, content)
+
+
+	@command()
+	async def flush(self, interaction: Interaction):
+		for file in os.listdir('dev_files'):
+			os.remove("dev_files/" + file)
+
+		status = ":x:"
+		if len(os.listdir('dev_files')) == 0: status = ":white_check_mark:"
+
+		await interaction.response.send_message(status, ephemeral=True)
+
+
+	@command()
+	async def edit(self, interaction: Interaction, name: str, new_content: str = None):
+		if not (new_content):
+			await interaction.response.send_modal(Edit_file(name))
+			return
+
+		await create_and_write(interaction, name, new_content)
+
+
+async def setup(bot: Bot):
+	await bot.add_cog(File(bot), guilds=[test_server])
---
+++ cogs/eval.py
@@ -0,0 +1,113 @@
+import sys
+
+from discord.app_commands import command, guilds, check
+from discord import Interaction, TextStyle, Embed
+from traceback import print_stack, format_exc
+from discord.ext.commands import Cog, Bot
+from discord.ui import Modal, TextInput
+from asyncio import get_event_loop
+from paginator import Paginator
+from mystbin import Client
+from io import StringIO
+
+from utils import test_server, is_owner, default_color
+
+
+mystbin_client = Client()
+
+
+async def evaluate_code(interaction: Interaction, code: str) -> str:
+	old_stdout = sys.stdout
+	redirected_output = sys.stdout = StringIO()
+
+	try: exec(code)
+	except Exception as e:
+		print_stack(file=sys.stdout)
+		print(sys.exc_info())
+
+	stdout = old_stdout
+
+	return str(redirected_output.getvalue())
+
+def format_output(_input: str, output: str) -> str:
+	return f"Input:\n```py\n{_input}\n```\nOutput:\n```cmd\n{output}\n```"
+
+
+class Eval_modal(Modal, title="Eval"):
+	code = TextInput(label="Code", style=TextStyle.paragraph, default="print()", required=True)
+
+	async def on_submit(self, interaction: Interaction):
+		if self.code == "print()":
+			await interaction.response.send_message('True', ephemeral=True)
+			return
+
+		output = await evaluate_code(interaction, self.code)
+
+		kwargs = {
+			'ephemeral': True
+		}
+
+		if len(output) < 2000:
+			await interaction.response.send_message(embed=Embed(description=output, color=default_color))
+			return
+
+		pages = []
+		for i in range(0, len(output), 2000 - len(self.code)):
+			new_output = ""
+			new_output = Embed(description=format_output(self.code, output[i:i+2000-len(code)]), color=default_color)
+			
+			pages.append(new_output)
+
+		if (new_output): pages.append(new_output)
+
+		await Paginator(interaction, pages).start(True)
+
+
+class Eval(Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@command()
+	@guilds(test_server)
+	@check(is_owner)
+	async def eval(self, interaction: Interaction, code: str = None, formatted: bool = True, ephemeral: bool = False, mystbin: bool = False, timeout: float = 300.0):
+		bot = self.bot
+
+		if (code):
+			output = await evaluate_code(interaction, code)
+
+			kwargs = {
+				"ephemeral": ephemeral
+			}
+
+			if (mystbin):
+				kwargs['content'] = await mystbin_client.post("# Input\n" + code + "\n\n\n\n# Output\n" + output, syntax="python")
+			else:
+				if len(output) > 2000:
+					pages = []
+					for i in range(0, len(output), 2000 - len(code)):
+						if not (formatted): new_output = output[i:i+2000-len(code)]
+						else: new_output = Embed(description=format_output(code, output[i:i+2000-len(code)]), color=default_color)
+
+						pages.append(new_output)
+
+					if (new_output): pages.append(new_output)
+
+					await Paginator(interaction, pages).start(formatted)
+					return
+
+				else:
+					if not (formatted): kwargs['content'] = output 
+					else: kwargs['embed'] = Embed(description=format_output(code, output), color=default_color)
+
+			await interaction.response.send_message(**kwargs)
+			return
+
+		modal = Eval_modal(timeout=timeout)
+		await interaction.response.send_modal(modal)
+		await modal.wait()
+
+
+async def setup(bot):
+	await bot.add_cog(Eval(bot))
