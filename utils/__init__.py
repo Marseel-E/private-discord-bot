d073be511f51368278efc5d24dcdd432b60354da
Marseel-E <marseeleeso@gmail.com>
2022-04-06 17:21:11 -0400

    add dungeon.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .tools import *
 from .checks import *
+from .dungeon import Dungeon
---
+++ utils/dungeon.py
@@ -0,0 +1,181 @@
+__all__ = ('Dungeon')
+
+from random import randint, choice
+from typing import Optional, List
+from dataclasses import dataclass
+from pprint import pprint
+from time import sleep
+
+
+@dataclass
+class Tile:
+	icon: str
+
+class Path(Tile):
+	def __init__(self) -> None:
+		super().__init__(icon="_")
+
+class Door(Tile):
+	def __init__(self, _type: str) -> None:
+		self.type = _type
+		super().__init__(icon="|")
+
+class Wall(Tile):
+	def __init__(self) -> None:
+		super().__init__(icon="[]")
+
+
+class Dungeon:
+	def __init__(self, rows: int = 10, columns: int = 10) -> None:
+		self.start = (0, 0)
+		self.end = (0, 0)
+
+		self.rows = rows
+		self.cols = columns
+
+		self.tiles = [[Wall() for i in range(self.cols)] for i in range(self.rows)]
+
+		self.generate_points()
+		self.generate_path()
+
+
+	def check_connection(self) -> bool:
+		"""Checks the connection between the start and end points
+
+		Returns:
+		-------
+			True <bool> - The two points are connected.
+			False <bool> - The two points are not connected.
+		"""
+		
+		s_x = self.start[0]
+		s_y = self.start[1]
+
+		e_x = self.end[0]
+		e_y = self.end[1]
+		
+		# Start point (UP, RIGHT, DOWN)
+		# End point (UP, LEFT, DOWN)
+
+		if (
+			(
+				((s_x > 0) and (self.tiles[s_x-1][s_y] == Path())) or
+				(self.tiles[s_x][s_y+1] == Path()) or
+				((s_x < (self.rows - 1)) and (self.tiles[s_x+1][s_y] == Path()))
+			) and (
+				((e_x > 0) and (self.tiles[e_x-1][e_y] == Path())) or
+				(self.tiles[e_x][e_y-1] == Path()) or
+				((e_x < (self.rows - 1)) and (self.tiles[e_x+1][e_y] == Path()))
+			)
+		): return True
+
+		return False
+
+	def check_walls(self) -> bool:
+		"""Checks if there are walls tiles still
+
+		Returns:
+		--------
+			True <bool> - There are wall tiles.
+			False <bool> - There aren't wall tiles.
+		"""
+
+		return ([Wall() for x in range(0, self.rows-1) for y in range(0, self.cols-1) if self.tiles[x][y] == Wall()])
+
+
+	def get_empty_tiles(self, x: int, y: int) -> Optional[List[tuple]]:
+		"""Fetches the empty/wall tiles around a point.
+
+		Parameters:
+		-----------
+			x <int> - The row of the point.
+			y <int> - The column of the point.
+
+		Returns:
+		--------
+			[(x, y), ...] <list> - A list of all the empty points coordinates. (can be empty)
+		"""
+
+		empty_tiles = []
+
+		# UP
+		if (x > 0) and (self.tiles[x-1][y] == Wall()):
+			empty_tiles.append((x-1, y))
+		# RIGHT
+		if (y < (self.cols - 1)) and (self.tiles[x][y+1] == Wall()):
+			empty_tiles.append((x, y+1))
+		# DOWN
+		if (x < (self.rows - 1)) and (self.tiles[x+1][y] == Wall()):
+			empty_tiles.append((x+1, y))
+		# LEFT
+		if (y > 0) and (self.tiles[x][y-1] == Wall()):
+			empty_tiles.append((x, y-1))
+
+		return empty_tiles
+
+
+	def generate_points(self) -> None:
+		"""Generates the start and end points"""
+
+		x, y = randint(0, self.rows-1), 0
+		self.start = (x, y)
+		self.tiles[x][y] = Door("start")
+
+		x, y = randint(0, self.rows-1), self.cols-1
+		self.end = (x, y)
+		self.tiles[x][y] = Door("end")
+
+	def generate_path(self) -> None:
+		"""Generates the path between the start and end points"""
+
+		while not self.check_connection():
+			if not (self.check_walls()):
+				break
+
+			for x in range(0, self.rows-1):
+				for y in range(0, self.cols-1):
+					if self.tiles[x][y] == Wall():
+						continue
+
+					empty_tiles = self.get_empty_tiles(x, y)
+
+					if not (empty_tiles):
+						continue
+
+					x, y = choice(empty_tiles)
+
+					if ((x, y) == self.start) or ((x, y) == self.end):
+						continue
+
+					if (
+						x not in [i for i in range(self.rows)] or
+						y not in [i for i in range(self.cols)]
+					): continue
+
+					self.tiles[x][y] = Path() # choice([Path(), Wall()])
+
+
+	def style_map(self) -> List[List[str]]:
+		"""Makes the map more readable
+
+		Returns:
+		--------
+			List[List[str]] <list> - A 2D array with strings instead of objects.
+		"""
+
+							# new_map = []
+							
+							# for row in range(self.rows):
+							# 	new_row = []
+
+							# 	for col in range(self.cols):
+							# 		new_row.append(self.tiles[row][col].icon)
+
+							# 	new_map.append(new_row)
+
+		return [[self.tiles[row][col].icon for col in range(self.cols)] for row in range(self.rows)]
+
+
+if __name__ == '__main__':
+	game = Dungeon()
+	pprint(game.style_map())
